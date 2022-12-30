b04ebde09fdc8419dfd0719afbd0ed51339cd1b5
Piotr Wosinek <piotr_wosinek@hotmail.com>
2022-12-30 01:30:48 +0100

    PL

--- lib/main_prod.dart
+++ lib/main_prod.dart
@@ -1,6 +1,7 @@
 import 'package:conferenceapp/config.dart';
 import 'package:conferenceapp/main_common.dart';
 
+//Moje dane
 void main() {
   final space = 'b486ph145db9';
   final apiKey = 'bH4fGddqJ1NdWI3YPO2DjBPMHoJWCL5u3N081mxu1Is';
